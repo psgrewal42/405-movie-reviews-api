b99b9678a9dd410c1f222491f5d489c1013e87e6
pgrewal <parminder_grewal@intuit.com>
2022-04-06 10:55:53 -0700

    add vader sentiment

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from typing import List
+
 import dash
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from rss_parser import Parser
 from requests import get
-import json
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 ########### Define a few variables ######
 from helpers.rss_utils import convert_feed_to_df
@@ -12,35 +14,53 @@ from helpers.rss_utils import convert_feed_to_df
 tabtitle = 'Vader News Sentiment'
 githublink = 'https://github.com/austinlasseter/tmdb-rf-classifier'
 
-
-
 ########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets: List[str] = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title = tabtitle
 
 ########### Layout
 
 app.layout = html.Div(children=[
+    html.Datalist(id='list-data', children=[html.Option(value="http://feeds.bbci.co.uk/news/rss.xml"),
+                                            html.Option(value=
+                                                        'http://rss.cnn.com/rss/cnn_topstories.rss'),
+                                            html.Option(value=
+                                                        'https://www.washingtontimes.com/rss/headlines/culture/health/'),
+                                            html.Option(value=
+                                                        'https://www.washingtontimes.com/rss/headlines/culture/technology/'),
+                                            html.Option(value=
+                                                        'https://www.washingtontimes.com/rss/headlines/opinion/editorials/')]),
     html.Div([
-        html.H3('Enter a RSS Feed'),
-        dcc.Input(id="input1", type="text", value="http://feeds.bbci.co.uk/news/rss.xml", className='six columns'),
+        html.H3('Enter a RSS Feed or Pick from list'),
+        dcc.Input(id="input1", type="text", value="", className='six columns',
+                  list='list-data'),
         html.Button('Submit', id='submit-btn', n_clicks=0, style={'marginLeft': '20px'}),
         html.Br(),
         html.Br(),
         html.Div(id="output1", children=[], className="nine columns")
     ], className='twelve columns'),
 
-
-        # Output
+    # Output
     html.Div([
         # Footer
         html.Br(),
         html.A('Code on Github', href=githublink, target="_blank")
     ], className='twelve columns'),
-    ]
+]
 )
+
+
+def max_value_sentiment(obj):
+    sentiment = ''
+    mx_val = 0
+    for sent in ['neg', 'pos', 'neu']:
+        if obj[sent] > mx_val:
+            mx_val = obj[sent]
+            sentiment = sent
+    return sentiment
+
 
 ########## Callbacks
 
@@ -49,32 +69,46 @@ app.layout = html.Div(children=[
               [Input('submit-btn', 'n_clicks')],
               State('input1', 'value'))
 def on_click(n_clicks, value):
-    xml = get(value)
-    parser = Parser(xml=xml.content)
-    feed = parser.parse()
-    df = convert_feed_to_df(feed.feed)
-    return dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
-                                , style_table={
-                                    'overflowY': 'scroll',
-                                    'overflowX': 'auto',
-                                    'width': 'auto'
-                                },
-                                style_cell={
-                                    'textAlign': 'left',
-                                    'minWidth': '150px',
-                                    'width': '500px',
-                                    'maxWidth': '600px',
-                                    'whiteSpace': 'normal'
+    if n_clicks == 0 or value == "":
+        return ""
+    try:
+        sanl = SentimentIntensityAnalyzer()
+        xml = get(value)
+        parser = Parser(xml=xml.content)
+        feed = parser.parse()
+        df = convert_feed_to_df(feed.feed)
+        df['sentiment'] = df.apply(lambda row: max_value_sentiment(sanl.polarity_scores(row['description'])), axis=1)
+        df['sentiment_tooltip'] = df.apply(lambda row: str(sanl.polarity_scores(row['description'])), axis=1)
+        return dash_table.DataTable(df.drop('sentiment_tooltip', axis=1).to_dict('records'),
+                                    [{"name": i, "id": i} for i in df.drop('sentiment_tooltip', axis=1).columns]
+                                    , style_table={
+                'overflowY': 'scroll',
+                'overflowX': 'auto',
+                'width': 'auto'
+            },
+                                    style_cell={
+                                        'textAlign': 'left',
+                                        'minWidth': '150px',
+                                        'width': '500px',
+                                        'maxWidth': '600px',
+                                        'whiteSpace': 'normal'
 
-                                },
-                                style_header={
-                                    'textAlign': 'center',
-                                    'fontWeight': 'bold'
-                                },
-                                page_size=10,
-                                fill_width=False
-                                )
-
+                                    },
+                                    style_header={
+                                        'textAlign': 'center',
+                                        'fontWeight': 'bold'
+                                    },
+                                    page_size=10,
+                                    fill_width=False,
+                                    tooltip_data=[
+                                        {
+                                            column: {'value': row['sentiment_tooltip'], 'type': 'markdown'}
+                                            for column, value in row.items() if column == 'sentiment'
+                                        } for row in df.to_dict('records')
+                                    ],
+                                    )
+    except:
+        return "Unable to parse this feed"
 
 
 ############ Deploy
